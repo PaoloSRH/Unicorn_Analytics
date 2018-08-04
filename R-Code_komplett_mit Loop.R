b26674aabaa726e478232d7e6f42b94c1c61868e
ValeriaRe <va.reifschneider@gmail.com>
2018-08-04 16:17:46 +0200

    Zusammenführung der verschiedenen Versionen

--- R-Code_komplett_mit Loop.R
+++ R-Code_komplett_mit Loop.R
@@ -1,20 +1,15 @@
+##### Choose option (MongoDB or CSV import) #####
+# MongoDB (=1) or csv import (=2)
+type <- 1
 
-# OPTIONEN
-
-# MongoDB (=1) or csv Import (=2)
-type <- 2
-
-# FastRText Einstellungen (Fuer Loop range angeben, Ergebnisse werden in Variable results gespeichert.)
+##### Set FastRText settings (Specifiy range for loop, results will be saved in variable 'results') #####
 epoche <- 128:128
 dime <- 27:27
 lr <- c(1)
 ngram <- 4:4
 verbose <-22:22
 
-#Einfach immer alles ausführen
-
-
-
+##### Load packages #####
 #install.packages("tm") 
 #install.packages("libxml2") 
 #install.packages("corpustools") 
@@ -28,7 +23,7 @@ library(tm)
 library(corpustools)
 library(fastrtext)
 
-# Remove Variables if they exist
+##### Remove variables if they exist #####
 if (exists("conn")) {rm(conn)}
 if (exists("getdata")) {rm(getdata)}
 if (exists("listid")) {rm(listid)}
@@ -55,19 +50,21 @@ if (exists("model")) {rm(model)}
 
 #rm(textframe3)
 
+##### Option 1: MongoDB #####
 if (type == 1) {
-# Verbindung zu Kategorien, wird aktuell nicht gebraucht
-# conn <- mongo("categories", url = "mongodb://192.168.2.135:27017/mails")  
+### Connection to categories
+#conn <- mongo("categories", url = "mongodb://192.168.2.135:27017/mails")  
 
-# Verbindung zur MongoDB zu den Mails:
-conn <- mongo("interactions", url = "mongodb://192.168.2.135:27017/mails")  
+### Connection to mails
+#conn <- mongo("interactions", url = "mongodb://192.168.2.135:27017/mails")
+conn <- mongo("interactions", url = "mongodb://localhost:27017/mails")
 getdata <- conn
 
-# Durchzählen wie viele Datensätze vorhanden sind
+### Count how many records are available
 getdata$count('{}')
 
-# Texte und Kategorien aus monodb und berinigen (da aktuell nicht sauber gespeichert)
-# Kategorien
+### Clean texts and categories of MongoDB
+# Categories
 listid <- getdata$find(query = '{}', fields = '{"categories.id" : true, "_id": false}')
 listid <- data.frame(lapply(listid, function(x) {gsub("list\\(id = \"", "", x)}))
 listid <- data.frame(lapply(listid, function(x) {gsub("\"\\)", "", x)}))
@@ -76,9 +73,7 @@ listid <- data.frame(lapply(listid, function(x) {gsub("\"", "", x)}))
 listid <- data.frame(lapply(listid, function(x) {gsub("\\)", "", x)}))
 listid <- data.frame(lapply(listid, function(x) {gsub("\n", "", x)}))
 
-
-
-# Relevante Textbausteine
+# Relevant text modules
 listtext <- getdata$find(query = '{}',fields = '{"categories.text" : true, "_id": false}')
 listtext <- data.frame(lapply(listtext, function(x) {gsub("list\\(text = \"", "", x)}))
 listtext <- data.frame(lapply(listtext, function(x) {gsub("\"\\)", "", x)}))
@@ -86,88 +81,80 @@ listtext <- data.frame(lapply(listtext, function(x) {gsub("\n", "", x)}))
 listtext <- data.frame(lapply(listtext, function(x) {gsub("\\)", "", x)}))
 
 
-#Dataframes zusammenführen und anpassen
+# Adapt and unite data frames
 textframe <- data.frame(listid, listtext)
 colnames(textframe) <- c("id", "text")
 
 }
 
-if (type == 2) {
+##### Option 2: csv file #####
+if (type == 2) { 
 csvimport = read.csv("maildaten2.csv", header = TRUE, sep=";", encoding = "UTF-8",stringsAsFactors = FALSE)
 textframe <- data.frame(csvimport[,10], csvimport[,12])
 colnames(textframe) <- c("id", "text")
 }
 
 
-
-# wir Laden die 2te Spalte in den Dataframe und verwenden tm zur Breinigung
+##### Load 2nd column in data frame and clean with tm #####
 corpus <- VCorpus(VectorSource(textframe[,2]))
 
-# inspect(corpus)
-#whitespace entfernen
+#inspect(corpus)
+# whitespace entfernen
 corpus <- tm_map(corpus, stripWhitespace)
-#convert to lowercases
+# convert to lowercases
 corpus <- tm_map(corpus, content_transformer(tolower))
-#remove stopwords
+# remove stopwords
 corpus <- tm_map(corpus, removeWords, stopwords("german"))
-#remove Punctuation
+# remove Punctuation
 corpus <- tm_map(corpus, removePunctuation)
-#replace contraction
+# replace contraction
 corpus <- tm_map(corpus, replace_contraction)
-#replace abbreviation
+# replace abbreviation
 corpus <- tm_map(corpus, replace_abbreviation)
-#replace symbol
+# replace symbol
 #corpus <- tm_map(corpus, replace_symbol)
-#stemming
+# stemming
 tm_map(corpus, stemDocument)
 
 corpusdataframe <- data.frame(text_new = sapply(corpus, as.character), stringsAsFactors = FALSE)
 
-
-
 #inspect(corpus)
 #print(textframe)
 
-## corpusmatrix erst einmal weglassen, wird nicht benötigt für fastrtext
-##Term document Matrices
+### corpusmatrix is not needed for FastRText
+# Term document matrices
 #corpusmatrix <- DocumentTermMatrix(corpus)
 #inspect(corpusmatrix)
 
-##Häufigkeitsmatrix
+# Frequency matrix
 #findFreqTerms(corpusmatrix, 10)
 
-##Korrelationen zwischen einzelnen Wörtern
+# Correlation between single words
 #findAssocs(corpusmatrix, "fehler", 0.5)
 
-##wir entfernen sehr seltene wörter, die unwichtig bzgl. der korrelation sind
+# Remove rare words
 #removeSparseTerms(corpusmatrix, 0.4)
 #inspect(corpusmatrix)
 
-#wir schreiben in eine neue spalte die editierten texte
+# Add new column with edited mail texts
 textframe[,3] <- corpusdataframe
 
+##### START OF: FASTRTEXT #####
 
-
-
-#FASTRTEXT
-
-#fuer fastrtext benötigen wir die daten in einem bestimmten format, dafür laden wir die benötigten spalten in 
-#ein neues dataframe
-
+### Data is needed in a specific format, load necessary columns in new dataframe
 textframe2 <- subset(textframe, select=c("id", "text_new"))
 names(textframe2) <- c("class.text", "text")
 
-# Wir mixen den Datensatz durch vor dem splitten
-# hier wäre besser 70/30 pro kategorie aber das muss man coden
-# wird nicht mehr benötigt: textframe2 <- textframe2[sample(nrow(textframe2)),]
+### Mix up dataset
+#textframe2 <- textframe2[sample(nrow(textframe2)),]
 
 
-if (type == 1) { #nur wenn MongoDB
-# Alle Zeilen mit mehreren IDs löschen, nicht sauber aber die müssen erst einmal weg
+if (type == 1) { #MongoDB
+### Delete rows with multiple IDs
 textframe2 <- textframe2[!grepl(",", textframe2$class.text),]
 
-#hier zähle ich einmalig die anzahl der texte pro kategorie um sie danach zu löschen
-#das muss man automatisieren
+### If there are not enough mail texts per category, categories will be deleted
+# TODO: Should be automated
 #textframe3 <- aggregate(cbind(count = text) ~ class.text, data = textframe2, FUN = function(x){NROW(x)})
 # delete cats 59, 70, 72, 85, 86, 87, 92, 97
 textframe2 <- textframe2[!grepl("59", textframe2$class.text),]
@@ -178,110 +165,119 @@ textframe2 <- textframe2[!grepl("86", textframe2$class.text),]
 textframe2 <- textframe2[!grepl("87", textframe2$class.text),]
 textframe2 <- textframe2[!grepl("92", textframe2$class.text),]
 textframe2 <- textframe2[!grepl("97", textframe2$class.text),]
-
 }
 
-#trainings und test datensätze
-#jede Kategorie wird 70/30 aufgeteilt
+### START OF training and test dataset generation ###
+textframe_ordered <- textframe2[order(textframe2$class.text, decreasing = FALSE), ]  
+textframe_ordered$class.text <- as.numeric(as.character(textframe_ordered$class.text))
+#str(textframe_ordered)
+#View(textframe_ordered)
+splits <- split(textframe_ordered, textframe_ordered$class.text)
+#View(splits)
 
-help_df1 <- data.frame(class.text=character(),text=character(),stringsAsFactors=FALSE)
-help_df2 <- data.frame(class.text=character(),text=character(),stringsAsFactors=FALSE)
 
-
-for(i in 1:length(unique(textframe2[,1]))){
-  temp_head <- head(subset(textframe2, class.text == unique(textframe2[,1])[i]),.7*nrow(subset(textframe2, class.text == unique(textframe2[,1])[i])))
-  temp_tail <- tail(subset(textframe2, class.text == unique(textframe2[,1])[i]),.3*nrow(subset(textframe2, class.text == unique(textframe2[,1])[i])))
-  
-  help_df1 <- rbind(help_df1, temp_head)
-  help_df2 <- rbind(help_df2, temp_tail)
-  
+### function to obtain first 70 percent of data (with round off)
+upperFunction <- function(textframes){
+  if(nrow(textframes)>0){
+    head(textframes, ceiling(nrow(textframes)*0.7))
+  }
 }
 
-train_sentences <- help_df1
-test_sentences <- help_df2
+### function to obtain last 30 percent of data (with round off)
+lowerFunction <- function(textframes){
+  if(nrow(textframes)>0){
+    tail(textframes, floor(nrow(textframes)*0.3))
+  }
+}
 
+### generating train and test dataset
+train_sentences_tmp <- lapply(splits, upperFunction)
+test_sentences_tmp <- lapply(splits, lowerFunction)
+#str(train_sentences)
+#str(test_sentences)
 
-#das muss verbessert werden
+### Combine data frames in list to one dataframe
+train_sentences <- bind_rows(train_sentences)
+test_sentences <- bind_rows(test_sentences)
+#str(train_set)
+#str(test_set)
+#View(train_set)
+#View(test_set)
+### END OF training and test dataset generation ###
+
+# TODO: Has to be improved
 #textframe2 %>% 
-#  add_count(class.text)
-
+#add_count(class.text)
 
 
 #for (year in c(2010,2011,2012,2013,2014,2015)){
 #  print(paste("The year is", year))
 #}
 
-#create dataframe to save results of the models
+### Create dataframe to save results of the models
 results <- setNames(data.frame(matrix(ncol = 8, nrow = 0)), c("id", "time", "dim","lr","ngram", "epoch","verbose", "result"))
 id <- 1
 
-
+##### FASTRTEXT Loop, ranges can be specified at the beginning of script #####
 for (i in epoche){ 
   for (j in dime){ 
     for (k in lr){ 
       for (l in ngram){ 
         for (m in verbose){ 
-
-
-
-
-# Daten in richtiges Format für Model
+          
+# Convert data in right format for model
 tmp_file_model <- tempfile()
-?tempfile()
 
-# label vor die kategorien
+### Train data set
+# Add "label" to categories
 train_labels <- paste0("__label__", train_sentences[,"class.text"])
-
-
-# Text in Kleinbuchstaben
+# Convert mail text in small letters
 train_texts <- tolower(train_sentences[,"text"])
 # View(train_texts)
 # Kategorie mit Prefix + Text aus Kleinbuchstaben
 train_to_write <- paste(train_labels, train_texts)
 # View(train_to_write)
 train_tmp_file_txt <- tempfile()
-# Daten in tempfile schreiben
+# Write data in tempfile
 writeLines(text = train_to_write, con = train_tmp_file_txt)
 
 
-# gleiches Vorgehen für Testdatensatz
+### Apply same procedure for test data set
 test_labels <- paste0("__label__", test_sentences[,"class.text"])
 test_labels_without_prefix <- test_sentences[,"class.text"]
 test_texts <- tolower(test_sentences[,"text"])
 test_to_write <- paste(test_labels, test_texts)
 
 
-# Modell trainieren und in tempfile abspeichern; Parameter können angepasst werden 
+### Train model and save in tempfile, parameter can be adjusted
 execute(commands = c("supervised", "-input", train_tmp_file_txt, "-output", tmp_file_model, "-dim", j, "-lr", k, "-epoch", i, "-wordNgrams", l, "-verbose", m))
-
 #execute(commands = c("supervised", "-input", train_tmp_file_txt, "-output", tmp_file_model, "-dim", 200, "-lr", 1, "-epoch", 20, "-wordNgrams", 2, "-verbose", 5))
 
-# Modell laden
+### Load model
 model <- load_model(tmp_file_model)
 
-# Modell auf Testdatensatz anwenden
+### Apply model for test data set
 predictions <- predict(model, sentences = test_to_write)
-# Vorhergesagte Kategorie und Wahrscheinlichkeit ausgeben
+### Vorhergesagte Kategorie und Wahrscheinlichkeit ausgeben
 #print(head(predictions,100))
 #summary(unlist(predictions))
-# Prozentualer Anteil, in dem das Model richtig lag
+### Prozentualer Anteil, in dem das Model richtig lag
 result <- mean(names(unlist(predictions)) == test_labels_without_prefix)
 
-# because there is only one category by observation, hamming loss will be the same
+### Because there is only one category by observation, hamming loss will be the same
 #get_hamming_loss(as.list(test_labels_without_prefix), predictions)
 
-# you can get flat list of results when you are retrieving only one label per observation
+# You can get flat list of results when you are retrieving only one label per observation
 #print(head(predict(model, sentences = test_to_write, simplify = TRUE)))
 
-#save results
+### Save results
 results[nrow(results) + 1,] = list(id,format(Sys.time(), format="%d.%m.%Y - %H:%M:%S"),j,k,l,i,m,result)
-
 
 id <- id+1
 
 show(result)
 
-# free memory and delete Files in Temp-Ordner
+### Free memory and delete files in temp folder
 # mit tempdir() auslesen welcher der Temp-Ordner ist, dann den Inhalt im nächsten Schritt immer löschen
 do.call(file.remove, list(list.files(tempdir(), full.names = TRUE)))
 unlink(train_tmp_file_txt)
@@ -289,3 +285,4 @@ unlink(tmp_file_model)
 gc()
 
 }}}}}
+##### END OF: FASTRTEXT #####
